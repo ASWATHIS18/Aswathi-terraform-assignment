ae1ee0fef028b213ab1999e1fdeee448d1b7a577
ASWATHIS18 <aswathiselvam.018@gmail.com>
2026-04-17 06:36:13 +0530

    added s3 and dynamoDB

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-aswathi-497645774924"
+    key            = "assignment/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.region
 }
